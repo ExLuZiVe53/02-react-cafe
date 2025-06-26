f811fe354253638250931a6d708eeeaf7042ee93
ExLuZiVe53 <imstudent27@gmail.com>
2025-06-27 00:19:15 +0200

    corected from state with App

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,33 +1,29 @@
-import css from "./VoteOptions.module.css";
+// import type { VoteType } from "../../types/votes";
+// import css from "./VoteOptions.module.css";
 
-export default function VoteOptions() {
-  // const onVote = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   console.log(event.type);
-  // console.log(vote);
-  // console.log(type);
-  // };
+// export default function VoteOptions(type: VoteType, vote: () => void) {
+//   const onVote = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     console.log(type);
+//     console.log(event);
+//   };
 
-  return (
-    <div className={css.container}>
-      <button
-        className={css.button}
-        // onClick={() => onVote(type)}
-      >
-        Good
-      </button>
-      <button
-        className={css.button}
-        // onClick={() => onVote(type)}
-      >
-        Neutral
-      </button>
-      <button
-        className={css.button}
-        // onClick={() => onVote(type)}
-      >
-        Bad
-      </button>
-      <button className={`${css.button} ${css.reset}`}>Reset</button>
-    </div>
-  );
-}
+//   return (
+//     <div className={css.container}>
+//       <button className={css.button} onClick={() => vote}>
+//         Good
+//       </button>
+//       <button className={css.button} onClick={() => vote}>
+//         Neutral
+//       </button>
+//       <button className={css.button} onClick={() => vote}>
+//         Bad
+//       </button>
+//       <button
+//         className={`${css.button} ${css.reset}`}
+//         // onClick={() => onReset()}
+//       >
+//         Reset
+//       </button>
+//     </div>
+//   );
+// }
